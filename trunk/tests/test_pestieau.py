0bafb688a3e2f911ade507a1d2c8f4bd65ed1b60
alan.isaac <alan.isaac@a76d3e55-6d33-0410-8a2f-b147e882df6a>
2007-06-28 21:48:17 +0000

    Add initial description of Population to agents.py.

--- trunk/tests/test_pestieau.py
+++ trunk/tests/test_pestieau.py
@@ -11,7 +11,7 @@ import random
 import sys
 sys.path.insert(0,'/econpy')  #need location of econpy
 from abs.pestieau1984oep import agents
-from pytrix import utilities
+from pytrix import utilities, iterate, fmath
 
 
 class Iterator4Test:
@@ -21,6 +21,17 @@ class Iterator4Test:
 		pass
 
 
+
+class test_iter(unittest.TestCase):
+	def test_bisect(self):
+		xint = 2.
+		f = lambda x: (x-xint)**3
+		itr = iterate.Bisect(f, xint-1.0, xint+1.0)
+		crit = iterate.AbsDiff(1e-9)
+		ip = iterate.IterativeProcess(itr, crit)
+		ip.run()
+		print "testvals", itr.get_testvals()
+		print "simple bisect", iterate.bisect(f, xint-1.0, xint+1.0)
 
 class testPestieau(unittest.TestCase):
 	def setUp(self):
@@ -63,8 +74,8 @@ class testPestieau(unittest.TestCase):
 		#test that two Gini formulae give same rsult
 		gini1 = utilities.calc_gini(self.wealths)
 		gini2 = utilities.calc_gini2(self.wealths)
-		print "gini1:%f, gini2:%f"%(gini1, gini2)
-		self.assert_(abs(gini1-gini2)<1e-8)
+		#print "gini1:%f, gini2:%f"%(gini1, gini2)
+		self.assert_(fmath.feq(gini1,gini2))
 	def test_Fund(self):
 		fund = agents.Fund(None)  #usu want association w economy
 		fund._accounts = [agents.FundAcct(fund, self.indivs[i], self.wealths[i]) for i in range(self.N)]
@@ -74,9 +85,15 @@ class testPestieau(unittest.TestCase):
 		N = random.randrange(100)
 		crit = lambda x,y: y>=N
 		it = Iterator4Test()
-		ip = agents.IterativeProcess(it, crit)
+		ip = iterate.IterativeProcess(it, crit)
 		ip.run()
 		self.assertEqual(ip.iterations,N)
+	def test_math(self):
+		print
+		print fmath.get_float_radix()
+		print fmath.get_machine_precision()
+		print fmath.get_default_numerical_precision()
+		print fmath.feq(1,2), fmath.feq(1e-9, 1e-10), fmath.feq(1e-16, 1e-17)
 
 if __name__=="__main__":
 	unittest.main()
