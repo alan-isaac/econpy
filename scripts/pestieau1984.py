fb4849905617dfaa54cb0b138d518e1d7a232195
kyle.caswell <kyle.caswell@a76d3e55-6d33-0410-8a2f-b147e882df6a>
2007-07-06 16:29:06 +0000

    update figure

--- scripts/pestieau1984.py
+++ scripts/pestieau1984.py
@@ -7,8 +7,8 @@ from econpy.abs.pestieau1984oep.agents import Indiv, PestieauCohort, Population
 from econpy.pytrix.iterate import IterativeProcess
 
 #for now, this is just illustrative; it's not "doing" anything
-COHORT_SIZE = 50
-N_COHORTS = 10
+COHORT_SIZE = 10
+N_COHORTS = 5
 individuals = [ Indiv(economy=None,sex=s) for i in range(COHORT_SIZE//2) for s in "MF" ]
 print ''.join(ind.sex for ind in individuals)
 cohort = PestieauCohort(individuals)
@@ -18,3 +18,13 @@ ppl = Population(PestieauCohort(Indiv(economy=None,sex=s) for i in range(COHORT_
 print "Pop element (a cohort, i.e., tuple of Indivs):"
 print ppl[0]
 
+
+#KC: just practice getting the classes do so something (not much yet, just testing my understanding):
+for t in range(4):
+	for cohorts in cohort:
+		cohort.set_age(0)
+		cohort.age_cohort()
+		print cohort._cohort_age
+		for ind in cohort:
+			ind.receive_income(2)
+			ind.calc_wealth()
