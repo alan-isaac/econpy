51cf192e9f438f5d5a2c4bc1b1c5b19bb85e0c0d
kyle.caswell <kyle.caswell@a76d3e55-6d33-0410-8a2f-b147e882df6a>
2007-07-10 15:56:59 +0000

    added notes

--- scripts/pestieau1984.py
+++ scripts/pestieau1984.py
@@ -6,8 +6,7 @@ from __future__ import division
 from random import random
 
 from scripts_config import econpy #get access to econpy package
-from econpy.abs.pestieau1984oep.agents import (
-PestieauEconomy, State, Population, PestieauCohort, Indiv,  Fund, FundAcct, PestieauParams)
+from econpy.abs.pestieau1984oep.agents import (PestieauEconomy, State, Population, PestieauCohort, Indiv,  Fund, FundAcct, PestieauParams)
 from econpy.pytrix.iterate import IterativeProcess
 
 #for now, this is just illustrative; it's not "doing" anything
@@ -29,7 +28,7 @@ print "Cohort element (an Indiv): ", cohort[0]
 
 print "#"*80
 print " Create Population ".center(80,'#')
-ppl = Population(PestieauCohort(Indiv(economy=None,sex=s) for i in range(COHORT_SIZE//2) for s in "MF") for j in range(N_COHORTS))
+ppl = Population( PestieauCohort( Indiv(economy=None,sex=s) for i in range(COHORT_SIZE//2) for s in "MF") for j in range(N_COHORTS) )
 print "Type of pop element (shd be a cohort): ", type(ppl[0])
 
 
@@ -60,19 +59,15 @@ print
 print "#"*80
 print " Example of Aging ".center(80,'#')
 for t in range(5):
-	print
+	print 'period %d:' %(t)
 	for i, cohort in enumerate(ppl):
 		cohort.age_cohort()
 		for ind in cohort:
 			#ind.open_account  #already initialized
-			ind.receive_income(100)	# not sure why I get a out of range error here
-		print "Indiv 0 of cohort %d has age %d and wealth of %5.2f."%(i, cohort[0].age, ind.calc_wealth())
+			ind.receive_income(100)
+		print "Indiv 0 of cohort %d has age %d and wealth of %5.2f."%( i, cohort[0].age, ind.calc_wealth() )
 	print
-
-
-
 
 #******************************
 #******** END ;-) *************
 #******************************
-
