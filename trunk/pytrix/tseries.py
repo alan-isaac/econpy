da115039bc6adc693caaa8edaae142239c9f25e6
alan.isaac <alan.isaac@a76d3e55-6d33-0410-8a2f-b147e882df6a>
2007-10-02 04:16:33 +0000

    Add simple Hodrick-Prescott filter to tseries.py. Improve descriptive statistics class Dstat1, now in stat.py.  Other minor changes.

--- trunk/pytrix/tseries.py
+++ trunk/pytrix/tseries.py
@@ -3,20 +3,20 @@
 :note: Run as __main__ to see example use.
 :author: Alan G. Isaac, except where otherwise specified.
 :since: 2004-08-04
-:date: 2005-08-23
+:date: 2007-08-23
 :copyright: 2005 Alan G. Isaac, except where otherwise specified.
 :license: `MIT license`_
 :see: pytrix.py
 :see: io.py
+:see: stat.py
 :see: pyGAUSS.py
-:see: tseries.py
 
 .. _`MIT license`: http://www.opensource.org/licenses/mit-license.php
 '''
+from __future__ import division, absolute_import
 __docformat__ = "restructuredtext en"
-import pytrix  #`series` subclasses `vector`
-reload(pytrix)
-import io
+
+#standard library imports
 import logging
 logging.getLogger().setLevel(logging.DEBUG) #sets root logger level
 #see http://www.python.org/doc/2.3.5/lib/node304.html to log to a file
@@ -29,6 +29,11 @@ import pylab
 from matplotlib.dates import date2num, MONTHLY, YEARLY
 import datetime
 import Tkinter as Tk
+
+#package imports
+from .pytrix import vector, vplus   #`series` subclasses `vector`
+from .io import freq2num, Sample, fetch
+from .stat import Dstat1
 
 have_numpy = False
 try:
@@ -82,17 +87,17 @@ Extensions
 '''
 
 
-class coef(pytrix.vector):
+class coef(vector):
 	def __init__(self,coefs):
 		self.data = list(coefs)  #name matters: see superclass methods
 		self.coefSE = None
 		self.length = len(self.data)
 		#result_class is for overriding result class in subclasses
-		self.result_class = pytrix.vector
+		self.result_class = vector
 		#core_attr: attributes to check conformability (see require_samecore)
 		self.core_attr = dict(length=self.length)
 
-class Series(pytrix.vplus):
+class Series(vplus):
 	'''Basic class for fixed frequency time series.
 	(Subclasses pytrix.vplus which subclasses pytrix.vector.)
 
@@ -110,7 +115,7 @@ class Series(pytrix.vplus):
 		elif isinstance(smpl,tuple):
 			assert(self.nobs==1+self.smpl_full[1]-self.smpl_full[0]), "Number of obs does not match sample."
 			self.smpl_full = smpl
-		elif isinstance(smpl,io.Sample):
+		elif isinstance(smpl,Sample):
 			assert len(data)==len(smpl.get_dates()), "Number of obs does not match sample."
 			self.smpl_full = smpl.copy()
 		else:
@@ -133,7 +138,7 @@ class Series(pytrix.vplus):
 	def stat(self):
 		# for comparable functionality see http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/409413
 		if not self.dstat:
-			self.dstat = dstat(self.data)
+			self.dstat = Dstat1(self.data)
 		keys = [
 		Mean,
 		Median,
@@ -163,7 +168,7 @@ class Series(pytrix.vplus):
 		if offset == 0:
 			return self.copy()
 		else:
-			return series(new_data,io.Sample(new_dates[0],new_dates[-1],freq=self.smpl_full.freq,dates=new_dates))
+			return series(new_data,Sample(new_dates[0],new_dates[-1],freq=self.smpl_full.freq,dates=new_dates))
 	def d(self,n=1,s=0):
 		'''Return (1-L)^n(1-L^s)x.'''
 		assert(s==int(s) and n==int(n)), "Fractional differences not supported."
@@ -180,14 +185,14 @@ class Series(pytrix.vplus):
 			logging.debug('series.d(): Modified tuple: '+str(dsmpl))
 		else:
 			old_dates = self.smpl_full.get_dates()
-			dsmpl = io.Sample(old_dates[n+s],
+			dsmpl = Sample(old_dates[n+s],
 							  old_dates[-1],
 							  freq=self.smpl_full.freq)
 			logging.debug('series.d(): Modified sample: '+str(dsmpl))
 		logging.debug("series.d(): n=%i,s=%i,dsmpl=%s"%(n,s,dsmpl))
 		return series(dnsx,smpl=dsmpl)
 	def transform(self,fn):
-		if isinstance(self.smpl_full,io.Sample):
+		if isinstance(self.smpl_full,Sample):
 			smpl_copy=self.smpl_full.copy()
 		else:
 			smpl_copy=self.smpl_full
@@ -202,7 +207,7 @@ class Series(pytrix.vplus):
 		intercept = itertools.cycle([1])
 		trend = xrange(self.nobs)
 		X = zip(intercept,trend)
-		return pytrix.Ols(self.data,X).get_resids()
+		return OLS(self.data,X).resids   #from ls.py
 	def plot(self, plottype='line', smpl=None):
 		if not smpl:
 			dates = self.smpl_full.get_dates()
@@ -210,7 +215,7 @@ class Series(pytrix.vplus):
 		else:
 			# specified sample may be outside data range: compensate
 			#TODO: fix for undated data
-			smpl_sub = io.Sample(max(smpl.start,self.smpl_full.start),
+			smpl_sub = Sample(max(smpl.start,self.smpl_full.start),
 			                  min(smpl.end,self.smpl_full.end),
 			                  self.smpl_full.freq)
 			dates = smpl_sub.get_dates()
@@ -238,7 +243,7 @@ class Series(pytrix.vplus):
 		data = self.data
 		if freq:
 			oldfreq = self.smpl_full.freq 
-			newfreq = io.freq2num(freq)
+			newfreq = freq2num(freq)
 			if newfreq == oldfreq:
 				pass
 			elif newfreq ==1 and oldfreq == 4:
@@ -251,7 +256,7 @@ class Series(pytrix.vplus):
 			if isinstance(smpl,tuple):
 				firstind = smpl[0]
 				lastind = smpl[1]
-			elif isinstance(smpl,io.Sample):
+			elif isinstance(smpl,Sample):
 				firstind = self.smpl_full.get_date_index(smpl.start)
 				lastind = self.smpl_full.get_date_index(smpl.end)
 			else:
@@ -264,7 +269,7 @@ class Series(pytrix.vplus):
 
 		:note: currently get_data already returns a list; see get_item in vector class
 		'''
-		assert(isinstance(self.smpl_full,io.Sample) and isinstance(smpl,io.Sample))
+		assert(isinstance(self.smpl_full,Sample) and isinstance(smpl,Sample))
 		assert (smpl.start >= self.smpl_full.start)and(smpl.end <= self.smpl_full.end)
 		return series(self.get_data(smpl,freq),smpl)
 
@@ -288,170 +293,6 @@ def show_tkagg(figure,title=''):
 
 
 
-class DstatSimple(object):
-	'''Simple descriptive statistics: no missing value handling!
-
-	References::
-
-		http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/409413
-		http://www.jennessent.com/arcview/sum_stats.htm
-		http://en.wikipedia.org/wiki/Skewness
-		http://en.wikipedia.org/wiki/Kurtosis
-
-	Available instance attributes::
-
-       nobs: total number of elements in the data set
-       sum:  sum of all values (n) in the data set
-       min:  smallest value of the data set
-       max:  largest value of the data set
-       mode: value(s) that appear(s) most often in the data set
-       mean: arithmetic average of the data set
-       zscores: vector of z-scores
-       #range:  difference between the largest and smallest value in the data set
-       median:  value which is in the exact middle of the data set
-       m2: measure of the spread of the data set about the mean: MLE
-       var: measure of the spread of the data set about the mean: unbiased
-       std: standard deviation - measure of the dispersion of the data set based on var
-       skew: sample skewness
-       kurtosis: sample kurtosis
-       jb: Jarque-Bera statistic
-       jbpval: Jarque-Bera statistic's pvalue (Chi^2 df=2)
-
-	:todo: direct support for series objects
-	:todo: add missing value handling? (but SciPy has this)
-	:note: Don't plan to add: Sum Sq. Dev.
-	'''
-	def __init__(self, v, name=None, smpl=None, mv=None):
-		if not v: raise ValueError('empty sample')
-		self.data = v #no copy!
-		self.id = id(self) #instance id
-		self.name = name
-		self.smpl = smpl
-		#from scipy.stats.stats import mean, variation, skew, kurtosis, histogram
-		self.__nobs = None
-		self.__sum = None
-		self.mean = self.sum/self.nobs
-		self.m2, self.m3, self.m4 = self.__get_cmoments(v)
-		# m2 is MLE variance estimate (biased)
-		# var is standard def of sample variance, not MLE estimate
-		try: self.var =  self.m2*self.nobs/(self.nobs-1)
-		except ZeroDivisionError: self.var = 0
-		self.std =  math.sqrt(self.var) #based on sample variance
-		self.__zscores = None   #delay calc until needed
-		self.__mode = None
-		self.median = self.__get_median(v)
-		self.skew = self.m3/math.sqrt(self.m2)**3
-		self.kurtosis = self.m4/self.m2**2			#NOT kurtosis excess (=kurtosis-3)
-		#Jarque-Bera: JB(X)=\frac{T}{6}\left[S^{2}(X) + \frac{[K(X)-3]^{2}}{4}\right]
-		self.jb = (self.skew**2 + (self.kurtosis-3)**2/4)*self.nobs/6
-		if have_scipy:
-			self.jbpval = scipy.stats.stats.chisqprob(self.jb,2) #check TODO
-		else:
-			self.jbpval = None
-		self.min = min(v)
-		self.max = max(v)
-		pylab.figure()
-		pylab.hist(v)
-		#self.valid =      K-vector, the number of valid cases.
-		#self.missing =     K-vector, the number of missing cases.
-	def __str__(self):
-		srep = '\n'
-		if self.name:
-			srep += 'Data:'.ljust(14) + self.name + '\n'
-		if self.smpl:
-			srep += 'Sample:'.ljust(14) + str(self.smpl) + '\n'
-		srep += 'Sample Size:'.ljust(14) + str(self.nobs) + '\n\n'
-		report = [
-		('Mean', self.mean),
-		('Variance',self.var),
-		('StdDev',self.std),
-		('Median',self.median),
-		]
-		report_fmt ="%+15s : %10.5f\n"
-		for x in report:
-			srep+=(report_fmt % x)
-		srep += '\n'
-		report = [
-		('Maximum',self.max),
-		('Minimum',self.min),
-		('Median',self.median),
-		('Skewness',self.skew),
-		('Kurtosis',self.kurtosis),
-		('Jarque-Bera',self.jb),
-		]
-		for x in report:
-			if x[1] is not None:
-				srep+=(report_fmt % x)
-		srep += "%+15s : %s" % ('Mode',' '.join(["%0.2f" % x for x in self.mode]))
-		return srep
-	def __get_nobs(self):
-		if self.__nobs is None:
-			self.__nobs = len(self.data)
-		return self.__nobs
-	nobs = property(__get_nobs,None,None,"Number of observations (length of series).")
-	def __get_sum(self):
-		if self.__sum is None:
-			self.__sum = float(sum(self.data))
-		return self.__sum
-	sum = property(__get_sum,None,None,"sum of series values")
-	def __get_zscores(self):
-		if self.__zscores is None:
-			self.__zscores = [(vi - self.mean)/self.std for vi in self.data]
-		return self.__zscores
-	zscores = property(__get_zscores,None,None,"z-scores for this series")
-	def __get_cmoments(self,v,m=[2,3,4]):
-		cmoments=[]
-		for xp in m:
-			cmoments.append(sum([(x-self.mean)**xp for x in v])/self.nobs) 
-		return tuple(cmoments)
-	def __get_median(self,v):
-		vs = v[:]
-		vs.sort()
-		median = not self.nobs%2 \
-		        and (vs[self.nobs//2-1]+vs[self.nobs//2])/2.0 \
-		        or  vs[self.nobs//2] #if even, return midpt
-		return median
-	def __get_mode(self):
-		# but see http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/277600
-		# for more efficient method using dictionary
-		if self.__mode is None:
-			v = self.data
-			mode = []
-			try: counts = [(v.count(x),x) for x in set(v)]
-			except NameError:
-				from sets import Set as set  #needed for Python 2.3
-				counts = [(v.count(x),x) for x in set(v)]
-			maxct = max(counts)[0]
-			if maxct > 1:
-				mode = [x[1] for x in counts if x[0]==maxct]
-			self.__mode = mode
-		return self.__mode
-	mode = property(__get_mode,None,None,"modal value of series")
-	def test(self):
-		logging.info( "Testing computations:" )
-		if have_numpy:
-			x = N.array(self.data)
-			assert(self.nobs == x.size)
-			assert(self.sum == x.sum())
-			assert(self.min == x.min())
-			assert(self.max == x.max())
-			assert(self.mean == x.mean())
-			assert(self.m2 == N.var(x))
-			assert(self.std == N.std(x))
-		if have_scipy:
-			assert(self.zscores == scipy.stats.zs(x))
-			assert(self.median == scipy.stats.median(x))
-			'''
-			#mode: value(s) that appear(s) most often in the data set
-		  #range: difference between the largest and smallest value in the data set
-			 m2: measure of the spread of the data set about the mean: MLE
-		   var: measure of the spread of the data set about the mean: unbiased
-		   std: standard deviation - measure of the dispersion of the data set based on var
-		   skew: sample skewness
-	   kurtosis: sample kurtosis
-			 jb: Jarque-Bera statistic
-		 jbpval: Jarque-Bera statistic's pvalue (Chi^2 df=2)
-			'''
 		
 def varlags(var,lags):
 	'''Prepare data for VAR. ::
@@ -478,15 +319,149 @@ def varlags(var,lags):
 	return N.array(var[lags:],copy=1),xlags
 
 
+
+#https://stat.ethz.ch/pipermail/r-help/2002-March/019283.html
+'''
+'''
+def hpfilter(y, penalty=1600):
+	'''Return: (t,d) trend and deviation
+	Based on Lubuele's GAUSS code:
+	http://www.american.edu/academic.depts/cas/econ/gaussres/timeseri/hodrick.src
+	which is a translation of Prescott's fortran code:
+	http://dge.repec.org/codes/prescott/hpfilter.for (Prescott's code)
+	which is a 2-pass Kalman filter.
+	assumes y is 1d
+	penalty is often called 'lambda'
+
+	Conceptualize the calculation as follows:
+	eye <- diag( length(y) )
+	d2 <- diff( eye, d=2 )
+	z <- solve( eye + penalty * crossprod(d2),  y )
+	'''
+	s = penalty
+	assert (s>0)
+	n = len(y)
+	assert (n>3)
+	t = [0]*n  #1d
+	d = [0]*n  #1d
+	v = N.zeros( (n,3) )
+	m1 = y[1]   #changed to zero-based indexing
+	m2 = y[0]   #changed to zero-based indexing
+	i1 = 3
+	i2 = n
+
+	#initialize v
+	v11 = 1
+	v22 = 1
+	v12 = 0
+	i = i1    #i initially 3, increments each pass
+	istep = 1
+	while (i <= i2): #first pass
+		#subroutine_pass()
+		x = m1
+		m1 *= 2
+		m1 -= m2
+		m2 = x
+		x = v11
+		z = v12
+		v11 = 1/s+4*(x-z)+v22
+		v12 = 2*x-z
+		v22 = x
+		dett = v11*v22-v12*v12
+		if istep == 1:  #counting fwd
+			v[i-2,0] = v22/dett #changed to zero-based indexing
+			v[i-2,2] = v11/dett #changed to zero-based indexing
+			v[i-2,1] = -v12/dett #changed to zero-based indexing
+			t[i-2] = v[i-2,0]*m1+v[i-2,1]*m2 #changed to zero-based indexing
+			d[i-2] = v[i-2,1]*m1+v[i-2,2]*m2 #changed to zero-based indexing
+		elif i >= 2: #counting backward
+			b11 = v11/dett
+			b12 = -v12/dett
+			b22 = v22/dett
+			e1 = b11*m2+b12*m1+t[i-1] #changed to zero-based indexing
+			e2 = b12*m2+b22*m1+d[i-1] #changed to zero-based indexing
+			b12 += v[i-1,1] #changed to zero-based indexing
+			b22 += v[i-1,2] #changed to zero-based indexing
+			b11 += v[i-1,0] #changed to zero-based indexing
+			dett = b11*b22-b12*b12
+			t[i-1] = (-b12*e1+b11*e2)/dett
+		x = v11+1
+		z = (y[i-1]-m1)/x #changed to zero-based indexing
+		m1 += v11*z
+		m2 += v12*z
+		z = v11
+		v11 -= v11*v11/x
+		v22 -= v12*v12/x
+		v12 -= z*v12/x
+		i += istep
+	t[-1] = m1  #ok
+	t[-2] = m2  #ok
+	m1 = y[-2]  #ok
+	m2 = y[-1]  #ok
+	i1 = n-2
+	i2 = 1
+	v11 = 1
+	v22 = 1
+	v12 = 0
+	i = i1
+	istep = -1
+	while (i >= i2): #second backward pass
+		#subroutine_pass()
+		x = m1
+		m1 *= 2
+		m1 -= m2
+		m2 = x
+		x = v11
+		z = v12
+		v11 = 1/s+4*(x-z)+v22
+		v12 = 2*x-z
+		v22 = x
+		dett = v11*v22-v12*v12
+		if istep == 1:  #counting fwd
+			v[i-2,0] = v22/dett #changed to zero-based indexing
+			v[i-2,2] = v11/dett #changed to zero-based indexing
+			v[i-2,1] = -v12/dett #changed to zero-based indexing
+			t[i-2] = v[i-2,0]*m1+v[i-2,1]*m2 #changed to zero-based indexing
+			d[i-2] = v[i-2,1]*m1+v[i-2,2]*m2 #changed to zero-based indexing
+		elif i >= 2: #counting backward
+			b11 = v11/dett
+			b12 = -v12/dett
+			b22 = v22/dett
+			e1 = b11*m2+b12*m1+t[i-1] #changed to zero-based indexing
+			e2 = b12*m2+b22*m1+d[i-1] #changed to zero-based indexing
+			b12 += v[i-1,1] #changed to zero-based indexing
+			b22 += v[i-1,2] #changed to zero-based indexing
+			b11 += v[i-1,0] #changed to zero-based indexing
+			dett = b11*b22-b12*b12
+			t[i-1] = (-b12*e1+b11*e2)/dett
+		x = v11+1
+		z = (y[i-1]-m1)/x #changed to zero-based indexing
+		m1 += v11*z
+		m2 += v12*z
+		z = v11
+		v11 -= v11*v11/x
+		v22 -= v12*v12/x
+		v12 -= z*v12/x
+		i += istep
+	t[0] = m1   #changed to zero-based indexing
+	t[1] = m2   #changed to zero-based indexing
+	i = 0   #changed to zero-based indexing
+	while i < n:   #changed to zero-based indexing
+		d[i] = y[i]-t[i]
+		i = i+1
+	return (t,d)
+
+
+
 if __name__ == "__main__":
 	'''Example use for module.
 	'''
-	cpi = series(*io.fetch(r'h:\data\FRED\cpi\CPIAUCNS.db'))
-	sub = io.Sample([1950,1],[1990,1],12)
+	cpi = series(*fetch(r'h:\data\FRED\cpi\CPIAUCNS.db'))
+	sub = Sample([1950,1],[1990,1],12)
 	print sub
 	cpi.plot()
 	cpi.plot(smpl=sub)
-	#print DstatSimple([1,1,2,2,3,3,3]Last modified: 2006 Jun 30
-	#print DstatSimple(cpi.get_data(),'cpi',cpi.smpl_full)
+	#print Dstat1([1,1,2,2,3,3,3]Last modified: 2006 Jun 30
+	#print Dstat1(cpi.get_data(),'cpi',cpi.smpl_full)
 	#pylab.show()
 
